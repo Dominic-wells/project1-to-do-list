fa1648dd528170a3033c84d55e203b3b588c1ac9
Dominic-wells <joedomwells@gmail.com>
2023-01-12 13:02:36 +0000

    Added a "make table" func

--- to-do-list-app.py
+++ to-do-list-app.py
@@ -6,3 +6,16 @@ import os
 db = "to-do-list.db"
 conn = sqlite3.connect(db)
 cursor = conn.cursor()
+
+#This will create a table in the database
+#Guidenace taken from https://www.sqlitetutorial.net/
+def makeTable():
+    query ="""CREATE TABLE tasks
+     (taskID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 
+    TaskName TEXT NOT NULL, 
+    TaskPriority INTEGER NOT NULL, 
+    TaskInfo TEXT NOT NULL,
+    Dateadded TIMESTAMP NOT NULL DEFAULT CURRENT_DATE,
+    TasksCompleted BOOLEAN NOT NULL DEFAULT FALSE)"""
+    cursor.execute(query)
+    conn.commit()
